3e46ec9daf3a2ca6765d2c9a0296873004cb9dc2
hs-2071034-kanghyeon <117580133+JioBani@users.noreply.github.com>
2023-11-23 20:19:22 +0900

    유저 서비스 추가, 회원가입시 유저 데이터 파이어베이스에 반영, 계정 화면에서 유저 이름과 생일 디스플레이

---
+++ app/src/main/java/com/hansung/androidusedtradeproject/model/AppUser.kt
@@ -0,0 +1,30 @@
+package com.hansung.androidusedtradeproject.model
+
+import android.icu.text.SimpleDateFormat
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentSnapshot
+
+data class AppUser(
+    val uid : String,
+    val email : String,
+    val name : String,
+    val birth : String
+) {
+
+    constructor(doc : DocumentSnapshot) :
+        this(
+            uid = doc["uid"].toString(),
+            email = doc["email"].toString(),
+            name = doc["name"].toString(),
+            birth = doc["birth"].toString()
+        )
+
+    fun makeHashMap(): HashMap<String, String> {
+        return hashMapOf(
+            "uid" to uid,
+            "email" to email,
+            "name" to name,
+            "birth" to birth
+        )
+    }
+}
